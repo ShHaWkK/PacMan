8c56f348d3ebb27622f656a7ffe2605141a56d4f
ShHawk <alexandreuzan9@gmail.com>
2024-01-14 03:42:43 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#include "../include/game.h"
+#include "../include/mouse.h"
+
+int main(int argc, char**argv) {
+    Game game;
+    initGame(&game);
+
+    while (game.isRunning) {
+        while (SDL_PollEvent(&game.event)) {
+            if (game.event.type == SDL_QUIT) {
+                game.isRunning = 0;
+            } else {
+                handleKeyboardInput(&game, &game.event);
+            }
+        }
+        updateGame(&game);
+        drawGame(&game);
+    }
+
+    closeGame(&game);
     return 0;
 }
